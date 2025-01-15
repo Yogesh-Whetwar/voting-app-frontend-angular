f86e8eb6d8444eecc8b80256af1a6ee74691b2bf
Yogesh Whetwar <yogicard69@gmail.com>
2025-01-15 12:15:38 +0530

    Interface and service creation is done till now

---
+++ src/app/poll.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Poll } from './poll.models';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PollService {
+
+private baseUrl = 'http://localhost:8080/api/polls';
+
+
+
+  constructor(private http:HttpClient) { 
+
+  }  
+ createPoll(poll:Poll):Observable<Poll>{
+   return this.http.post<Poll>(this.baseUrl,poll);
+ }
+
+ getPolls():Observable<Poll[]>{
+   return this.http.get<Poll[]>(`${this.baseUrl}/getAll`);
+ }
+
+ 
+  
+  vote(pollId:number, optionIndex:number):Observable<void>{
+    return this.http.post<void>(`${this.baseUrl}/vote`, { pollId,optionIndex});
+  }
+
+}
